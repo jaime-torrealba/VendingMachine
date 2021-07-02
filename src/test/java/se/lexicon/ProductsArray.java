478661c73fe9de8f44506d63859d80284e835dfd
deltagare <jantool@icloud.com>
2021-07-02 23:10:26 +0200

    Updated structure

---
+++ src/test/java/se/lexicon/ProductsArray.java
@@ -0,0 +1,15 @@
+package se.lexicon;
+
+class productsArray {
+
+    public static void main(String[] args) {
+        int Fruit = 3;
+        int Drinks = 3;
+        int Snack = 3;
+
+        System.out.println("apple, pear, banana" + Fruit);
+        System.out.println("ginger-ale, orange-juice , mineral_water" + Drinks);
+        System.out.println("apple, pear, banana" + Snack);
+    }
+
+}
