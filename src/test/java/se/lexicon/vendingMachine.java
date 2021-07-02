4bfa1bc7cc7b6d43ee44e5f65cb8248d005fe324
deltagare <jantool@icloud.com>
2021-07-02 14:46:09 +0200

    Initial commit VendingMachine structure

---
+++ src/test/java/se/lexicon/vendingMachine.java
@@ -0,0 +1,16 @@
+package se.lexicon;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class vendingMachine {
+    public static void main(String[] args) {
+
+        // Calculates if certain of things complies.
+        boolean calculate = true;
+        while (calculate) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Welcome, choose your product");
+        }
+    }
+}
