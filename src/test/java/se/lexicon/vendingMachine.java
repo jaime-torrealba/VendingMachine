ac58457e2c63f27cd8de30aa9a05cb8a37311bd3
deltagare <jantool@icloud.com>
2021-07-03 00:07:31 +0200

    Updated structure 2

--- src/test/java/se/lexicon/vendingMachine.java
+++ src/test/java/se/lexicon/vendingMachine.java
@@ -10,7 +10,68 @@ public class vendingMachine {
         boolean calculate = true;
         while (calculate) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Welcome, choose your product");
+            System.out.println("Welcome! Please, choose your product");
+
+            class products {
+                        static final int FRUIT_PRICE = 7;
+                        static final int FRUIT_CAPACITY = 30;
+
+                        int getFruitPrice;
+                        int balance;
+                        int revenue;
+
+                        VendingMachine() {
+                            getFruitPrice = FRUIT_CAPACITY;
+                            balance = 0;
+                            revenue = 0;
+
+                            class Snacksautomat {
+                                static final int CANDY_PRICE = 5;
+                                static final int CANDY_CAPACITY = 30;
+
+                                int snickers;
+                                int balance;
+                                int revenue;
+
+                                VendingMachine() {
+                                    snickers = CANDY_CAPACITY;
+                                    balance = 0;
+                                    revenue = 0;
+                }
+
+                int getBalance() {
+                    return balance;
+                }
+
+                int getRevenue() {
+                    return revenue;
+                }
+
+                void addMoney() {
+                    Balance++;
+                }
+
+                int refund() {
+                    int amount = balance;
+                    balance = 0;
+                    return amount;
+                }
+
+                boolean vendCandyBar() {
+                    if(candyBars >= 1 && balance >= CANDY_PRICE) {
+                        candyBars--;
+                        balance-= CANDY_PRICE;
+                        revenue+= CANDY_PRICE;
+                        return = true;
+                    } else }
+                  return false;
+            }
+        }
+
+        void restock(); {
+            candyBars = CANDY_CAPACITY;
+        }
+    }
         }
     }
 }
